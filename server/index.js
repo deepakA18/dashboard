555f2d6dafabc3a562cc2f5d8e32ea65eb5ca110
deepakagashe18 <agashedeepak55@gmail.com>
2024-01-22 11:04:49 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+
+require('dotenv').config({path: '.env'})
+const connectDB = require('./db/conn')
+
+
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json()); 
+
+const start = async () => {
+    try {
+        await connectDB(process.env.URI)
+            app.listen(port,()=> {
+                console.log(`server running on port ${port}...`);
+            })
+        
+    } catch (error) {
+        console.log('Error connecting database...!',error)
+    }
+}
+
+start();
